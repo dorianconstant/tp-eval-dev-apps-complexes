d58a022e59507c0c303dd5e4f8e5c1a822810fac
Constant Dorian <21703363t>
2019-03-14 10:15:04 +0100

    ajout JSDoc à CompoTaskList.js

--- js/CompoTaskList.js
+++ js/CompoTaskList.js
@@ -1,16 +1,50 @@
 class CompoTaskList {
+    /**
+     * @class
+     * @summary Composant qui affiche une liste de tâches
+     * Les paramètres obligatoire, à transmettre sous la forme
+     * d'un objet, sont :
+     * - task : objet Task à afficher
+     * - element : objet HTMLElement du DOM dans lequel
+     *   le code HTML doit être affiché
+     * @param {*} param Objet de paramétrage
+     * @example
+     * let tk = new Task('test', null);
+     * let compoTk = new CompoTask({
+     *    task: tk,
+     *    element: document.getElementById('task')
+     * }) 
+     * compoTk.render();
+     */
     constructor(param) {
         this.param = param;
     }
 
+    /**
+     * @method
+     * @returns Retourne la liste de tâches passée en paramètre au constructeur
+     */
     getTaskList() {
         return this.param.taskList;
     }
 
+    /**
+     * @method
+     * @returns Retourne l'élément du DOM passé en paramètre au constructeur
+     */
     getElement() {
         return this.param.element;
     }
 
+    /**
+     * Créer une variable children contenant un tableau vide
+     * Si l'élément du DOM passé en paramètre au constructeur existe :
+     *      il l'affiche dans la console
+     *      puis il boucle autant de fois qu'il y a de tâches dans la liste
+     *      il stocke une div dans la variable (let) item
+     *      il ajoute l'enfant item à la div
+     *      il créer un objet
+     */
     render() {
         let children = [];
         if (this.param.taskList) {
